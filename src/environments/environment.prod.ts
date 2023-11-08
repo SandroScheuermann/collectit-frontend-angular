94fc678689452dc6f519a647a83bdb77b4cab8e9
Sandro Müller Scheuermann <sandroryze@gmail.com>
2023-11-08 15:30:02 -0300

    ajustando link da api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://collectit-webapi.fly.dev/'
+  apiUrl: 'https://muscler-webapi.fly.dev/'
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://collectit-webapi.fly.dev/'
+  apiUrl: 'https://muscler-webapi.fly.dev/'
 };
